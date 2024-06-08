42af066def0c8fd9d1b1b8efb652685ff72631b6
sepehrhero7 <sepehr779977@yahoo.com>
2024-06-08 10:04:35 +0330

    templates

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STRIPE_SECRET_KEY = 'your_secret_key'
+
+STRIPE_PUBLISHABLE_KEY = 'your_publishable_key'
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Product, Cart, CartItem
+from django.contrib.auth.decorators import login_required
+from .models import Product, Cart, CartItem, Order, OrderItem
 
 def product_list(request):
     products = Product.objects.all()
@@ -21,3 +22,12 @@ def add_to_cart(request, product_id):
 def cart_detail(request):
     cart, created = Cart.objects.get_or_create(user=request.user)
     return render(request, 'store/cart_detail.html', {'cart': cart})
+
+@login_required
+def checkout(request):
+    cart = get_object_or_404(Cart, user=request.user)
+    order = Order.objects.create(user=request.user)
+    for item in cart.cartitem_set.all():
+        OrderItem.objects.create(order=order, product=item.product, quantity=item.quantity)
+    cart.cartitem_set.all().delete()
+    return render(request, 'store/checkout.html', {'order': order})
